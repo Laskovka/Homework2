471396c9c5cf3541a43821b9f89419ee3ba809e1
Laskovka <laskovskiu2017@gmail.com>
2020-12-08 18:44:08 +0200

    first commit

---
+++ IsNumberEvenOrNot.java
@@ -0,0 +1,10 @@
+public class IsNumberEvenOrNot {
+    public static void main(String[] args) {
+        int number = 7;
+        if(number % 2 == 0) {
+            System.out.println("Number \"" + number + "\" is even.");
+        } else {
+            System.out.println("Number \"" + number + "\" isn't even.");
+        }
+    }
+}
---
+++ LowerModulus.java
@@ -0,0 +1,17 @@
+public class LowerModulus {
+    public static void main(String[] args) {
+        double numberOne = 3.9;
+        double numberTwo = -6.4;
+        double numberThree = -3.7;
+        double moduleOfNumberOne = numberOne > 0 ? numberOne : numberOne * -1;
+        double moduleOfNumberTwo = numberTwo > 0 ? numberTwo : numberTwo * -1;
+        double moduleOfNumberThree = numberThree > 0 ? numberThree : numberThree * -1;
+        if (moduleOfNumberOne <= moduleOfNumberTwo && moduleOfNumberOne <= moduleOfNumberThree) {
+            System.out.println(moduleOfNumberOne);
+        } else if (moduleOfNumberTwo <= moduleOfNumberOne && moduleOfNumberTwo <= moduleOfNumberThree) {
+            System.out.println(moduleOfNumberTwo);
+        } else {
+            System.out.println(moduleOfNumberThree);
+        }
+    }
+}
